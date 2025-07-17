fa4e565700d5e8ddd283e8ddb0e2c1a1c6efd67e
Abhay17R <abhayjh132@gmail.com>
2025-07-17 14:10:34 +0545

    roll_back

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -1,21 +1,17 @@
 export const sendToken = async (user, statusCode, message, res) => {
   const token = await user.generateToken();
 
+  // Sirf zaruri fields frontend ko bhejo
   const { _id, name, email, phone, role, familyId, parentId } = user;
-
-  const cookieOptions = {
-    expires: new Date(
-      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production", // HTTPS ke liye
-    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
-    partitioned:true,
-  };
 
   res
     .status(statusCode)
-    .cookie("token", token, cookieOptions)
+    .cookie("token", token, {
+      expires: new Date(
+        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true,
+    })
     .json({
       success: true,
       message,
