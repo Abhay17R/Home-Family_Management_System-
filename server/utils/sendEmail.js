e55c0c49ad7452a52e84b9952191c87ca6015ff8
Abhay17R <abhayjh132@gmail.com>
2026-03-03 09:21:00 +0545

    signup_fixed

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,36 +2,28 @@ import nodeMailer from "nodemailer";
 import { config } from "dotenv";
 config({ path: 'config.env' });
 
-// Security Warning: Production me credentials log mat karna
-// console.log("username and password", process.env.SMTP_MAIL, process.env.SMTP_PASSWORD) 
-
 export const sendEmail = async ({ email, subject, message }) => {
-    
     const transporter = nodeMailer.createTransport({
-        host: process.env.SMTP_HOST, // smtp.gmail.com
-        port: process.env.SMTP_PORT, // Render Env me 587 daalna
-        
-        // IMPORTANT CHANGE: 587 ke liye false, 465 ke liye true
+        host: process.env.SMTP_HOST, 
+        port: process.env.SMTP_PORT, 
         secure: process.env.SMTP_PORT == 465, 
-        
-        // service: process.env.SMTP_SERVICE, // Ise comment kar do, Host/Port better hai
-        
         auth: {
-            user: process.env.SMTP_MAIL,
-            pass: process.env.SMTP_PASSWORD,
+            user: process.env.SMTP_MAIL, 
+            pass: process.env.SMTP_PASSWORD, 
         },
-        // TLS issue fix karne ke liye (kabhi kabhi Render ke naye IP trusted nahi hote)
         tls: {
             rejectUnauthorized: false 
         }
     });
 
     const options = {
-        from: process.env.SMTP_MAIL,
+        
+        from: `"Home Verify" <${process.env.SENDER_EMAIL}>`, 
         to: email,
         subject,
         html: message,
     };
 
     await transporter.sendMail(options);
+    console.log(`Email successfully sent to ${email} via Brevo`);
 };
